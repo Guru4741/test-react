b9d215b9baef1965f6e9490d24ec11309da17aba
Apple <apple@Apples-MacBook-Air.local>
2021-01-17 21:45:48 +0530

    feat: Changed Headline

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Monica Bellucci
         </a>
       </header>
     </div>
